5a37c792387ce20bc883c2765215f5859810beaf
prtfnx <prtfnx@gmail.com>
2019-08-02 01:39:48 +0300

    Card shuffle and fix

--- DeckClass.py
+++ DeckClass.py
@@ -18,6 +18,7 @@ class Deck:
 
 
     def create(self, deck_name = ""):
+        # make new deck with all cards
         random_id=1 # TODO
         if (deck_name == ""):
             deck_name = self.name
@@ -27,18 +28,22 @@ class Deck:
                 self.card_list.append(Card(line,random_id,index,))
         
     def sort(self):
-        pass  
+        # sort deck randomly
+        pass  #TODO
 
     def get_card(self,position):
+        # take one card by position 
         return self.card_list[position]
 
 
     def delete(self):
+        # delete all cards
         self.card_list=[]
         #self.sqlite.connect()
         #self.sqlite.delete_deck(self.name, self.client_id)
         #self.sqlite.disconnect()
     def select(self, deck_name = ""):
+        # load deck from database 
         self.card_list=SQlite.get_list_of_cards
 
         #if (deck_name == ""):
--- test1.py
+++ test1.py
@@ -1,10 +1,27 @@
 from DeckClass import Deck
+from CardShuffle import card_shuffle
+from CardClass import Card
 
-
-def main():
+def deck_create_test():
     new_deck=Deck('thoth',1)
     new_deck.create()
     second_card=new_deck.card_list[1]
     print(new_deck.get_card(2).name)
-    
-main()
+
+
+def shuffle_test():
+    list_test=[]
+    result=[]
+    for i in range(78):
+        list_test.append(Card(str(i),i,i))
+    for card in list_test:
+        result.append(card.name)
+    print(','.join(result))
+    result=[]
+    card_shuffle(list_test)
+    for card in list_test:
+        result.append(card.name)
+    print(','.join(result))
+
+
+shuffle_test()
